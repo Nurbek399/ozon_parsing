fd02de9bb7bcff47303d45aca6f658172d9a7f5d
Nurbek399 <nurbek05012006@gmail.com>
2025-02-28 18:58:37 +0600

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from functions import collect_product_info
-import psycopg2
 
 
 def init_webdriver():
